cbfe48b5927306969eba2951078b81afb656e8c0
Parba <iiuccsestudent@gmail.com>
2023-05-13 21:38:57 +0600

    project init

---
+++ student_management_system/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .import views,Hod_Views,Staff_Views,Student_Views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('base/',views.BASE,name='base'),
+
+    #login path
+    path('login/',views.LOGIN,name='login'),
+
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
