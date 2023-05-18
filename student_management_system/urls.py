5440c1166912dd0bdf35898c826f5e5316d63c25
Parba <iiuccsestudent@gmail.com>
2023-05-18 22:00:09 +0600

    Dashboard

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -11,7 +11,13 @@ urlpatterns = [
     path('base/',views.BASE,name='base'),
 
     #login path
-    path('login/',views.LOGIN,name='login'),
+    path('',views.LOGIN,name='login'),
+    path('doLogin',views.doLogin,name='doLogin'),
+    path('doLogout',views.doLogout,name='logout'),
+
+
+    #This is Hod Panel url
+    path('Hod/Home',Hod_Views.HOME,name='hod_home'),
 
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
