75aeb6c6f2af3777d5f8347684919d14ef0e223b
Parba <iiuccsestudent@gmail.com>
2023-06-14 21:12:21 +0600

    dashboard

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -15,9 +15,29 @@ urlpatterns = [
     path('doLogin',views.doLogin,name='doLogin'),
     path('doLogout',views.doLogout,name='logout'),
 
+    #profile Update
+    path('Profile',views.PROFILE,name='profile'),
+    path('Profile/update',views.PROFILE_UPDATE,name='profile_update'),
+    
 
     #This is Hod Panel url
     path('Hod/Home',Hod_Views.HOME,name='hod_home'),
+    path('Hod/Student/Add',Hod_Views.ADD_STUDENT,name='add_student'),
+    path('Hod/Student/View',Hod_Views.VIEW_STUDENT,name='view_student'),
+    path('Hod/student/Edit/<str:id>',Hod_Views.EDIT_STUDENT,name='edit_student'),
+    path('Hod/student/Update',Hod_Views.UPDATE_STUDENT,name='update_student'),
+
+    path('Hod/Course/Add',Hod_Views.ADD_COURSE,name='add_course'),
+    path('Hod/Course/View',Hod_Views.VIEW_COURSE,name='view_course'),
+    path('Hod/Course/Edit/<str:id>',Hod_Views.EDIT_COURSE,name='edit_course'),
+    path('Hod/Course/Update',Hod_Views.UPDATE_COURSE,name='update_course'),
+    path('Hod/Course/Delete/<str:id>',Hod_Views.DELETE_COURSE,name='delete_course'),
+    
+    path('Hod/Subject/Add',Hod_Views.ADD_SUBJECT,name='add_subject'),
+    path('Hod/Subject/View',Hod_Views.VIEW_SUBJECT,name='view_subject'),
+    path('Hod/Subject/Edit/<str:id>',Hod_Views.EDIT_SUBJECT,name='edit_subject'),
+
+
 
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- student_management_system/views.py
+++ student_management_system/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect,HttpResponse
 from app.EmailBackEnd import EmailBackEnd
 from django.contrib.auth import authenticate,logout,login
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from app.models import  CustomUser
 
 
 
@@ -40,6 +42,51 @@ def doLogin(request):
 def doLogout(request):
     logout(request)
     return redirect('login')
+
+@login_required(login_url='/')
+def PROFILE(request):
+    user=CustomUser.objects.get(id = request.user.id)
+   
+
+    context={
+        "user":user,
+    }
+    return render(request,'profile.html',context)
+
+
+@login_required(login_url='/')
+def PROFILE_UPDATE(request):
+    if request.method == "POST":
+        profile_pic = request.FILES.get('profile_pic')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        #email = request.POST.get('email')
+        #username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(profile_pic)
+       
+        try:
+            customuser=CustomUser.objects.get(id = request.user.id)
+
+            customuser.first_name=first_name
+            customuser.last_name=last_name
+            
+            
+            if password !=None and password != "":
+                customuser.set_password(password)
+            if profile_pic !=None and profile_pic != "":
+                customuser.profile_pic=profile_pic
+            customuser.save()
+            messages.success(request,'Your Profile Updated Successfully! ')
+            return redirect('profile')
+
+        except:
+            messages.error(request,'Failed to update your profile')
+
+
+        return render(request,'profile.html')
+
+
 
 
             
